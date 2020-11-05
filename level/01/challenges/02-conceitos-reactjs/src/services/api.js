862496099304ec626f55dc2ee57836e8d4a8584d
wyllbassos <bassoscolorado@gmail.com>
2020-11-05 13:23:28 +0000

    add baseurl: gitpod

--- level/01/challenges/02-conceitos-reactjs/src/services/api.js
+++ level/01/challenges/02-conceitos-reactjs/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  //baseURL: "http://localhost:3333",
+  baseURL: "https://3333-e9a9f3ca-773d-43c5-b2be-f54380eb04b2.ws-us02.gitpod.io/"
 });
 
 export default api;
