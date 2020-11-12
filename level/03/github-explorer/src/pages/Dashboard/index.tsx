fc48f07cbb7efa8ebb5d17f2767bfd97b3c18c5c
Wylliam Bassos <bassoscolorado@gmail.com>
2020-11-12 06:11:57 -0300

    v0.0
    Inicio Parte 03 Modulo 01 - Iniciando GitHub-Explorer

---
+++ level/03/github-explorer/src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import Title from './styles';
+
+const Dashboard: React.FC = ( ) => {
+    return <Title>Explore Repositórios no GitHub</Title>
+}
+
+export default Dashboard
---
+++ level/03/github-explorer/src/pages/Dashboard/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+//Templeta Literals
+const Title = styled.h1`
+    font-size: 48px;
+    color: #3a3a3a;
+`;
+
+export default Title;
