62a3811a34a31bdcdd4c9927336e05a96b35d8cf
Wylliam Bassos <bassoscolorado@gmail.com>
2020-12-07 05:23:38 -0300

    V11.2.1
    Iniciado Servico de envio de email de recurepacao de senha

--- gobarber/backend/src/shared/container/providers/index.ts
+++ gobarber/backend/src/shared/container/providers/index.ts
@@ -3,6 +3,9 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
