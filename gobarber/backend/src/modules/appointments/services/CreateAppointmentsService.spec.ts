9974927924683d50fb572f6a0508499772c57979
Wylliam Bassos <bassoscolorado@gmail.com>
2020-12-22 11:28:29 -0300

    V11.3.6.3
    Refatorado teste de Criação de agendamento

--- gobarber/backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ gobarber/backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,15 +1,20 @@
 import AppError from '@shared/errors/AppError';
-import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import FakeNotificationsRepository from '@modules/notifications/repositories/fakes/FakeNotificationsRepository';
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentsService';
 
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
 let createAppointment: CreateAppointmentService;
+let fakeNotificationsRepository: FakeNotificationsRepository;
 
 describe('CreateAppointmentService', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    fakeNotificationsRepository = new FakeNotificationsRepository();
+
     createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
+      fakeNotificationsRepository,
     );
   });
 
