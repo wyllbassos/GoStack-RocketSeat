1e1bac6903dcd9acfe80c6605cf1e5b8003509eb
Wylliam Bassos <bassoscolorado@gmail.com>
2021-01-25 22:09:00 -0300

    V11.3.9
    Adicionado rateLimiter para delimitar a quantidade de requests que um ip pode executar em um certo periodo de tempo para evitar ataques DDoS

--- gobarber/backend/src/shared/infra/http/server.ts
+++ gobarber/backend/src/shared/infra/http/server.ts
@@ -12,9 +12,11 @@ import '@shared/container';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
 import routes from './routes';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
