eef05ec960bb7f33f2f268cc355309c322210972
Wylliam Bassos <bassoscolorado@gmail.com>
2021-02-07 08:28:26 -0300

    V11.4.4
    -App Mobile
    --Finalizado lista de providers na pagina de Provider Appointments

--- gobarber/mobile/src/pages/AppointmentDatePicker/index.tsx
+++ gobarber/mobile/src/pages/AppointmentDatePicker/index.tsx
@@ -11,6 +11,11 @@ import {
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styles';
 
 export interface Provider {
@@ -32,6 +37,23 @@ const AppointmentDatePicker: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
   const navigation = useNavigation();
+  const params = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [availability, setAvailability] = useState<AvailabilityItem[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState<string>(
+    params.providerId,
+  );
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
 
   return (
     <>
@@ -43,6 +65,25 @@ const AppointmentDatePicker: React.FC = () => {
 
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+      <Container>
+        <ProvidersListContainer>
+          <ProvidersList
+            data={providers}
+            keyExtractor={provider => provider.id}
+            renderItem={({ item: provider }) => (
+              <ProviderContainer
+                selected={provider.id === selectedProvider}
+                onPress={() => handleSelectProvider(provider.id)}
+              >
+                <ProviderAvatar source={{ uri: provider.avatar_url }} />
+                <ProviderName selected={provider.id === selectedProvider}>
+                  {provider.name}
+                </ProviderName>
+              </ProviderContainer>
+            )}
+          />
+        </ProvidersListContainer>
+      </Container>
     </>
   );
 };
--- gobarber/mobile/src/pages/AppointmentDatePicker/styles.tsx
+++ gobarber/mobile/src/pages/AppointmentDatePicker/styles.tsx
@@ -54,3 +54,41 @@ export const UserAvatar = styled.Image`
   border-radius: 28px;
   margin-left: auto;
 `;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(
+  FlatList as new () => FlatList<Provider>,
+).attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingVertical: 32,
+    paddingHorizontal: 24,
+  },
+})``;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+
+  background: ${props => (props.selected ? '#FF9000' : '#3e3b47')};
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 36px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  margin-left: 8px;
+  color: ${props => (props.selected ? '#232129' : '#f4ede8')};
+`;
